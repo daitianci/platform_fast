3dd1958f6b2581a7c2e72d0aa133feb3e90eaacb
Honor <502395193@qq.com>
2019-08-04 10:01:22 +0800

    junit

---
+++ src/test/java/com/honor/platform/TestUnit.java
@@ -0,0 +1,18 @@
+package com.honor.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by 戴天赐 on 2019/8/4.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestUnit {
+    @Test
+    public void test(){
+        System.out.println("aaaa");
+    }
+}
